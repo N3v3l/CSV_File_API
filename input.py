3b1acd2ff0346084829c36990d0816514b7dc770
N3v3l <39237686+N3v3l@users.noreply.github.com>
2020-04-27 13:53:18 +0200

    Update input.py

--- input.py
+++ input.py
@@ -3,12 +3,20 @@
 # Ressources: 
 
 import numpy as np
-#import pandas as pd
+import pandas as pd
 
-#!curl -O Nicolass-MacBook-Pro/Mac/Users/Nicolas 1/Desktop/shared/Test.csv
-#ls -l
+!curl -O https://raw.githubusercontent.com/DJCordhose/ml-examples/master/datasets/Iris/iris_dirty.csv
+ls -l
+
+df = pd.read_csv('iris_dirty.csv',
+                 header=None,
+                 encoding='iso-8859-15',
+                 names=['sepal length', 'sepal width', 'petal length', 'petal width', 'class'])
+
 
 from farmware_tools import get_config_value, device
 
-INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
+df.loc[82,'sepal width'] = value
+
+INPUT_VALUE = get_config_value(farmware_name='CSV_File_API', config_name='input', value_type=str)
 device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
