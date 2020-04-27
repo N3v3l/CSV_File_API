165848f85af797eb9a0e17ad7c4d23fe263a31d3
N3v3l <39237686+N3v3l@users.noreply.github.com>
2020-04-27 11:58:34 +0200

    Update input.py

--- input.py
+++ input.py
@@ -13,10 +13,7 @@ import numpy as np
                  encoding='iso-8859-15',
                  names=['temp', 'hummidity', 'co2', 'phase', 'time'])
 
-df.loc
-
 from farmware_tools import get_config_value, device
-
 
 INPUT_VALUE = get_config_value(farmware_name='Hello Farmware Input', config_name='input', value_type=str)
 device.log(message='Hello Farmware! Input was: {}'.format(INPUT_VALUE), message_type='success')
